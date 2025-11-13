0404bc82ed00884ccc43f7ba5369292acce2d682
Guilherme Rios <110192250+guiirios@users.noreply.github.com>
2025-11-13 09:43:26 -0300

    Update littleCalculator.py
    Adding the method init to assign values to the object properties.

--- littleCalculator.py
+++ littleCalculator.py
@@ -1,17 +1,20 @@
-#MY Calculator
 class Calculator:
-    def add(self, *args): 
+    #Init is making a default to the methods, as also, assign value to object properties without being manually
+    def __init__(self, *args):
+        self.args = args
+
+    def add(self): 
         #acessing the first number
-        result = args[0] 
-        for num in args[1:]: 
+        result = self.args[0] 
+        for num in self.args[1:]: 
            result += num 
         return result
     
-    def substract(self, *args):
+    def substract(self):
         #saving the first number put on args
-        result = args[0]
+        result = self.args[0]
         #while true, the loop will acess each number from args through the num
-        for num in args[1:]:
+        for num in self.args[1:]:
             #updating the result, before it was a number, now its a new number 
             result -= num
         return result
@@ -23,17 +26,13 @@ class Calculator:
         return result
     
     def divide(self, *args):
-        result = args[0]
+        result = self.args[0]
         #it will take from the first one because result = args[0] is already taking the first one 
-        for num in args[1:]:
+        for num in self.args[1:]:
             result /= num
         return result
 
 
 #bringing the "character" to life(instancing)
-cal1 = Calculator()
-cal1.add(4, 5)
-
-
-cal2 = Calculator()
-cal2.divide(2, 1)
+cal1 = Calculator(1,2, 4)
+cal1.add()
